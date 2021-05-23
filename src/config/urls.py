4870193246c15c9fb3ac531d7a06684188b7b1a1
austinlin <austin89213@gmail.com>
2021-05-23 14:57:23 +0000

    feat : add working list tags endpoint

--- src/config/urls.py
+++ src/config/urls.py
@@ -20,4 +20,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('users.urls')),
+    path('api/reciper/', include('recipes.urls')),
 ]
--- src/core/tests/test_models.py
+++ src/core/tests/test_models.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='test@gmail.com', password='testpass'):
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successfull(self):
@@ -32,3 +38,11 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_superuser(email='test@london.com', password='test')
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """  Test the tag string representation """
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan',
+        )
+        self.assertEqual(str(tag), tag.name)
